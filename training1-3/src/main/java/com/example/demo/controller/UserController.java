1d06a8381f34358145023dc477b8534322e41978
81806 <81806@192.168.10.101>
2025-02-14 17:02:07 +0900

    未完成

---
+++ training1-3/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.dto.UserRequest;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(value="/user/list")
+	public String displayList(Model model) {
+		List<User>userlist=userService.searchAll();
+		model.addAttribute("userlist",userlist);
+		return "user/list";
+	}
+	
+	@GetMapping(value ="/user/add")
+	public String displayAdd(Model model) {
+		model.addAttribute("userRequest", new UserRequest());
+		return "user/add";
+	}
+	@RequestMapping(value="/user/create",method=RequestMethod.POST)
+	public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+		
+	 if(result.hasErrors()) {
+		 
+		 List<String>errorList= new ArrayList<String>();
+		 for(ObjectError error : result.getAllErrors()) {
+			 errorList.add(error.getDefaultMessage());
+		
+		 }
+		 model.addAttribute("ValidationError", errorList);
+		 return "user/add";
+	 }
+	 userService.create(userRequest);
+	 return "redirect:/user/list";
+	 
+	}
+	 @GetMapping ("/user/{id}")
+	 public String displayView(@PathVariable Long id, Model model) {
+		 return "user/view";
+		
+	}
+	
+	}
+
